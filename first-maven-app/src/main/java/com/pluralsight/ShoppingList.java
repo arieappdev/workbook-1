66efd77de42a9f612f6fa9f9d4c9bf9fd5d02c1f
Arie Fisher <jbgirl0@gmail.com>
2025-04-10 11:18:51 -0400

    completed ShoppingList app

---
+++ first-maven-app/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("ice cream");
+        System.out.println("bread");
+        System.out.println("gouda");
+        System.out.println("brie");
+        System.out.println("cheddar");
+        System.out.println("proscitto");
+        System.out.println("geona salami");
+        System.out.println("dark chocolate");
+        System.out.println("spinach artichoke dip");
+        System.out.println("crackers");
+        System.out.println("honey");
+        System.out.println("peach jam");
+        System.out.println("pistachios");
+        System.out.println("cornichons");
+
+    }
+}
